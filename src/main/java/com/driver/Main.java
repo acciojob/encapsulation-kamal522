95815142e550883e7d580bc1bb9e664f99b94919
kamal jena <kamaljena57@gmail.com>
2022-10-10 19:04:27 +0530

    Encapsulation code working

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main (String[]args){
+        RWOnly obj=new RWOnly();
+//        obj.name="tushar";
+//        java: name has private access in com.driver.RWOnly
+        obj.setName("tushar");
+        System.out.println(obj.getName());
+    }
 }
